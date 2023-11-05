ecc789dfb6aa24d65068ed357bd93bfaa5a22393
Varshini <varshini.sanakr@cdw.com>
2023-11-05 15:08:34 +0530

    Made change

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
-app.use(bodyParser.json);
-app.use(bodyParser.urlencoded({extended: true}));
 const port = 3001;
 var mysql = require('mysql');
 var con = mysql.createConnection({
